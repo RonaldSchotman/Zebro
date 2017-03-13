adea6d889c573af9d8d9182f6f85107b5966e968
Marty253 <Martijn.de.Rooij.1996@gmail.com>
2017-03-13 08:22:05 +0100

    Starting in new file PZDemo_OpenCV for better results and calibration techniques

---
+++ PicoZebroDemo/PZDemo_OpenCV.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# TU Delft Pico Zebro Demo
+# PZ_DEMO recognition determination. (This code is for recognizing Pico Zebro)
+# Writer: Martijn de Rooij
+# Version 0.03
+
+# Everything is being tested from 120 cm
