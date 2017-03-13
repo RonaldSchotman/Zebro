6fea80cf328a67e3392cb75d56224436717ef855
Marty253 <Martijn.de.Rooij.1996@gmail.com>
2017-03-13 10:57:45 +0100

    Calibration in PZ_Demo_Opencv after Calibration testing setup 1 (not working yet)

--- PicoZebroDemo/Calibrate.py
+++ PicoZebroDemo/Calibrate.py
@@ -13,7 +13,7 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*.jpg')
+images = glob.glob('Calibration/*.jpg')
 
 for fname in images:
     img = cv2.imread(fname)
@@ -33,5 +33,33 @@ for fname in images:
         img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
         cv2.imshow('img',img)
         cv2.waitKey(500)
+
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+np.savez("calibration_ouput_2", ret=ret, mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+
+# Calibration Functions
+ret, mtx, dist, rvecs, tvecs = np.load('calibration_ouput_1.npz')
+
+img = cv2.imread("Calibration/left01.jpg")
+h,  w = img.shape[:2]
+(newcameramtx, roi) = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult.png',dst)
+
+mean_error = 0
+tot_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    tot_error = tot_error + error
+
+print ("total error: ", mean_error/len(objpoints))
 
 cv2.destroyAllWindows()
--- PicoZebroDemo/PZDemo_OpenCV.py
+++ PicoZebroDemo/PZDemo_OpenCV.py
@@ -32,3 +32,48 @@ camera = PiCamera()
 camera.resolution = (1648, 928)
 camera.framerate = 20
 rawCapture = PiRGBArray(camera, size=(1648, 928))
+
+# Calibration Functions
+data = np.load('calibration_ouput_2.npz')
+(ret, mtx, dist, rvecs, tvecs) = (data['ret'], data['mtx'], data['dist'], data['rvecs'], data['tvecs'])
+
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the NumPy array representing the image, the initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+
+    h,w = image.shape[:2]
+    (newcameramtx, roi) = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+    # undistort
+    dst = cv2.undistort(image, data['mtx'], data['dist'], None, newcameramtx)
+    # undistort V2
+    mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
+    dst = cv2.remap(image,mapx,mapy,cv2.INTER_LINEAR)
+
+    # crop the image
+    x,y,w,h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv2.imshow("dst" ,dst)
+
+    #Use of calibrated image
+
+#calibration_ouput_1
+
+    # Take current day
+    Timetest = time.strftime("%d-%m-%Y")
+    # Show the current view for debugging
+    cv2.imshow("original %s" % Timetest,image)
+
+    # show the frame
+    key = cv2.waitKey(1) & 0xFF
+
+    #clear the stream in preparation of the next frame
+    rawCapture.truncate(0)
+
+    # if the 'q' key was pressed, break from the loop
+    if key == ord("q"):
+        # cleanup the camera and close any open windows
+        cv2.destroyAllWindows()
+        break
